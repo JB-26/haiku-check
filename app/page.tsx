b5a44307c1e0d5de516a46902976ef9418de4f1d
Joshua Blewitt <joshblewitt@protonmail.com>
2025-12-01 18:23:10 +0000

    Some more styling and changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Link from "next/link";
 import checkHaiku from "./checkHaiku";
 import countSyllables from "./countSyllables";
 import famousHaiku from "./famousHaikus";
 import junicode from "./junicode.module.css";
+
+type Haiku = {
+  lineOne: string;
+  lineTwo: string;
+  lineThree: string;
+  author: string;
+};
 
 // TODO: pick a theme from DaisyUI or components from Shadcn
 // TODO: add a favicon
 // TODO: write tests in jest and (possibly) in playwright
 
 export default function Home() {
+  // union type - it can be a type of Haiku or null, just in case Haiku doesn't exist yet
+  const [randomHaiku, setRandomHaiku] = useState<Haiku | null>(null);
   // this is being stored in state so it doesn't get re-rendered on every keystroke
-  const [randomHaiku] = useState(() => famousHaiku());
   const [line1, setLine1] = useState("");
   const [line2, setLine2] = useState("");
   const [line3, setLine3] = useState("");
@@ -21,6 +30,13 @@ export default function Home() {
     line3: 0,
   });
   const [haiku, setHaiku] = useState<boolean>();
+
+  useEffect(() => {
+    if (!randomHaiku) {
+      // eslint-disable-next-line
+      setRandomHaiku(famousHaiku());
+    }
+  }, [randomHaiku]);
 
   const readInput = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -38,16 +54,16 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className={`text-6xl ${junicode.junicode}`}>
+    <div className="flex flex-col items-center justify-center w-2xl m-auto">
+      <h1 className={`text-4xl md:text-6xl m-2.5 ${junicode.junicode}`}>
         Wait, is that a{" "}
         <span className={`text-orange-400 ${junicode.headerHaiku}`}>haiku</span>
         ?
       </h1>
-      <h2 className={`text-4xl ${junicode.junicode}`}>
+      <h2 className={`text-3xl md:text-4xl m-2.5 ${junicode.junicode}`}>
         Have you ever thought that?
       </h2>
-      <p className={`text-3xl ${junicode.junicode}`}>
+      <p className={`text-xl md:text-3xl m-2.5 ${junicode.junicode}`}>
         Have you ever had the blind panic that you didn&apos;t know that the
         haiku was not a haiku? Well, I built this tool to help you answer the
         question,{" "}
@@ -115,14 +131,50 @@ export default function Home() {
             <div></div>
           )}
         </div>
-        <h2>Random Haiku - refresh the page to see a different one!</h2>
-        <div className={`${junicode.junicodeItalic}`}>
-          <p>{randomHaiku.lineOne}</p>
-          <p>{randomHaiku.lineTwo}</p>
-          <p>{randomHaiku.lineThree}</p>
-          <p>-{randomHaiku.author}</p>
-        </div>
       </form>
+      <h2>Random Haiku - refresh the page to see a different one!</h2>
+      <div
+        className={`flex flex-col items-center justify-center w-2xl m-auto text-2xl p-2.5 ${junicode.junicodeItalic}`}
+      >
+        {randomHaiku ? (
+          <>
+            <p>{randomHaiku.lineOne}</p>
+            <p>{randomHaiku.lineTwo}</p>
+            <p>{randomHaiku.lineThree}</p>
+            <p>-{randomHaiku.author}</p>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+      <div className="m-2.5">
+        <p>
+          I built this project for two reasons. One, I thought it was fun. Two,
+          I wanted to challenge myself and learn more about forms, states and
+          effects in NextJS and React.
+        </p>
+        <br />
+        <p>
+          At a company I worked for, the team I was part of had a tradition of
+          writing haikus when we were due to give an update at a team meeting.
+          But, there were several occasions when we wrote something that
+          wasn&apos;t a haiku! So I decided to build this checker that would
+          decide if something was a haiku.
+        </p>
+      </div>
+      <footer>
+        <p>
+          A fun tool built by{" "}
+          <Link
+            href="https://www.joshblewitt.dev/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="underline hover:text-orange-400"
+          >
+            Joshua Blewitt
+          </Link>
+        </p>
+      </footer>
     </div>
   );
 }
